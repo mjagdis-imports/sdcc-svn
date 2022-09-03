388a3b275bca398ba04d505c8a0a9f1a684e10dd
felixs <felixs@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2022-09-03 22:35:08 +0000

    Revert a compile warning fix in hash-table.h
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/cpp@13689 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/cpp/gcc/hash-table.h
+++ sdcc/support/cpp/gcc/hash-table.h
@@ -956,12 +956,6 @@ hash_table<Descriptor, Lazy, Allocator>
 ::find_slot_with_hash (const compare_type &comparable, hashval_t hash,
 		       enum insert_option insert)
 {
-#if 1 // sdcpp
-	(void) comparable;
-	(void) hash;
-	(void) insert;
-	gcc_assert(0 && "unreachable");
-#else // sdcpp
   if (Lazy && m_entries == NULL)
     {
       if (insert == INSERT)
@@ -1022,7 +1016,6 @@ hash_table<Descriptor, Lazy, Allocator>
 
   m_n_elements++;
   return &m_entries[index];
-#endif // sdcpp
 }
 
 /* Verify that all existing elements in th hash table which are
@@ -1054,10 +1047,6 @@ void
 hash_table<Descriptor, Lazy, Allocator>
 ::remove_elt_with_hash (const compare_type &comparable, hashval_t hash)
 {
-#if 1 // sdcpp
-	(void) comparable;
-	(void) hash;
-#else // sdcpp
   value_type *slot = find_slot_with_hash (comparable, hash, NO_INSERT);
   if (slot == NULL)
     return;
@@ -1066,7 +1055,6 @@ hash_table<Descriptor, Lazy, Allocator>
 
   mark_deleted (*slot);
   m_n_deleted++;
-#endif // sdcpp
 }
 
 /* This function scans over the entire hash table calling CALLBACK for
