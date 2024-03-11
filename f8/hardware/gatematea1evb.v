7e491ca39f0af3db423ead1d28b77e1c8d3f9b26
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-03-11 14:30:35 +0000

    Infrastructure for trying f8 on GateMateA1-EVB FPGA board.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8@14724 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ f8/hardware/gatematea1evb.v
@@ -0,0 +1,60 @@
+`include "system.v"
+
+`begin_keywords "1800-2009"
+
+// Test module for use on GateMateA1-EVB FPGA board.
+
+// Divide clock by 2.
+module clkgen (output clk, input CLK);
+	reg clk2;
+	always @(posedge CLK)
+	begin
+		clk2 = !clk2;
+	end
+	always @(posedge clk2)
+	begin
+		clk = !clk;
+	end
+endmodule
+
+module gatematea1evb (input logic CLK,
+	inout tri PMOD_1, inout tri PMOD_2, inout tri PMOD_3, inout tri PMOD_4, inout tri PMOD_7, inout tri PMOD_8, inout tri PMOD_9, inout tri PMOD_10,
+	input logic BTN_N, inout tri RX, inout tri TX);
+	wire [7:0] gpio0pins, gpio1pins, gpio2pins;
+	wire clk;
+	wire trap;
+	wire power_on_reset;
+
+	// Reset button
+	assign power_on_reset = !BTN_N;
+
+	// PMOD2
+	assign PMOD_7 = gpio0pins[0];
+	assign gpio0pins[0] = PMOD_7;
+	assign PMOD_1 = gpio0pins[1];
+	assign gpio0pins[1] = PMOD_1;
+	assign PMOD_3 = gpio0pins[2];
+	assign gpio0pins[2] = PMOD_3;
+	assign PMOD_2 = gpio0pins[3];
+	assign gpio0pins[3] = PMOD_2;
+	assign PMOD_8 = gpio0pins[4];
+	assign gpio0pins[4] = PMOD_8;
+	assign PMOD_9 = gpio0pins[5];
+	assign gpio0pins[5] = PMOD_9;
+	assign PMOD_4 = gpio0pins[6];
+	assign gpio0pins[6] = PMOD_4;
+	assign PMOD_10 = gpio0pins[7];
+	assign gpio0pins[7] = PMOD_10;
+
+	// Serial
+	assign TX = gpio2pins[0];
+	assign gpio2pins[0] = TX;
+	assign RX = gpio2pins[1];
+	assign gpio2pins[1] = RX;
+
+	clkgen clkgen(.*);
+	system system(.*);
+endmodule
+
+`end_keywords
+
