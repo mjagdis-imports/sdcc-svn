be5f6069790cb978102bbccad55b27a603a8f62e
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-03-20 10:13:33 +0000

    C source for tests.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8@14734 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ f8/hardware/tests/cblink/test.c
@@ -0,0 +1,42 @@
+// A 5 bit LED second counter.
+
+#include <stdint.h>
+
+#define IRQCTRLEN (*((volatile uint8_t *)0x0010))
+#define IRQCTRLACT (*((volatile uint8_t *)0x0012))
+
+#define TIMER0CTRL (*((volatile uint8_t *)0x0018))
+#define TIMER0CNT (*((volatile uint16_t *)0x001a))
+#define TIMER0RLD (*((volatile uint16_t *)0x001c))
+
+#define GPIO0DDR (*((volatile uint8_t *)0x0028))
+#define GPIO0ODR (*((volatile uint8_t *)0x002a))
+
+uint16_t counter = 64536;
+
+void interrupthandler(void) __interrupt(0)
+{
+	counter++;
+	if(!counter)
+	{
+		counter = 64536;
+		GPIO0ODR++; // Increment second counter on LEDs once per second.
+	}
+	IRQCTRLACT = 0; // Clear interrupt request.
+}
+
+void main(void)
+{
+	// Set up GPIO0
+	GPIO0ODR = 0;    // Switch off all LEDs.
+	GPIO0DDR = 0x1f; // Set the lines connected to LEDs as output.
+
+	// Set up TIMER0 for 1 KHz interrupt assuming 3 MHz system clock.
+	TIMER0CNT = 62536;
+	TIMER0RLD = 62536;
+	IRQCTRLEN = 0x01;  // Enable timer 0 overflow interrupt.
+	TIMER0CTRL = 0x01; // Start timer.
+
+	for(;;);
+}
+
---
+++ f8/hardware/tests/chello/test.c
@@ -0,0 +1,66 @@
+// "Hello, world!" at 9600 baud.
+
+#include <stdint.h>
+#include <signal.h>
+#include <stdio.h>
+
+#define IRQCTRLEN (*((volatile uint8_t *)0x0010))
+#define IRQCTRLACT (*((volatile uint8_t *)0x0012))
+
+#define TIMER0CTRL (*((volatile uint8_t *)0x0018))
+#define TIMER0CNT (*((volatile uint16_t *)0x001a))
+#define TIMER0RLD (*((volatile uint16_t *)0x001c))
+
+#define GPIO2DDR (*((volatile uint8_t *)0x0038))
+#define GPIO2ODR (*((volatile uint8_t *)0x003a))
+
+volatile unsigned char sendcounter;
+volatile unsigned int senddata;
+volatile sig_atomic_t sending;
+
+void send_bit(void) __interrupt(0)
+{
+	GPIO2ODR = senddata & 1;
+	senddata >>= 1;
+
+	if(!--sendcounter)
+	{
+		sending = 0;
+
+		IRQCTRLEN &= ~0x01;  // Disable timer 0 overflow interrupt.
+	}
+
+	IRQCTRLACT = 0; // Clear interrupt request.
+}
+
+int putchar(int c)
+{
+	while(sending);
+
+	senddata = (c << 1) | 0x200;
+
+	sendcounter = 10;
+
+	sending = 1;
+
+	IRQCTRLEN = 0x01;  // Enable timer 0 overflow interrupt.
+
+	return (c);
+}
+
+void main(void)
+{
+	// Set up GPIO2
+	GPIO2ODR = 0x01;
+	GPIO2DDR = 0x01;
+
+	// Set up TIMER0 for 9.6 KHz interrupt assuming 3 MHz system clock.
+	TIMER0CNT = 65224;
+	TIMER0RLD = 65224;
+	TIMER0CTRL = 0x01; // Start timer.
+
+	printf("Hello, World!\n");
+
+	for(;;);
+}
+
