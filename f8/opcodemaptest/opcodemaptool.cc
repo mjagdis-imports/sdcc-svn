19bb5b44daddab48d354895b86b62d5ce88ac1cd
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-07-16 15:09:03 +0000

    Revise f8 opcode map experiments infrastructure.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8@14922 4a8a32a2-be11-0410-ad9d-d568d2c75423

---
+++ f8/opcodemaptest/opcodemaptool.cc
@@ -0,0 +1,522 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <vector>
+#include <utility>
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <filesystem>
+#include <random>
+
+#include <sys/random.h>
+#include <sys/stat.h>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/vector.hpp>
+
+#define NUM_OPCODES 256
+
+const char *opcodenames[NUM_OPCODES] =	{
+	"OPCODE_TRAP",
+	"OPCODE_SUB_XL_DIR",
+	"OPCODE_SUB_XL_SPREL",
+	"OPCODE_SUB_XL_ZREL",
+	"OPCODE_SUB_XL_ZL",
+	"OPCODE_SUB_XL_XH",
+	"OPCODE_SUB_XL_YL",
+	"OPCODE_SUB_XL_YH",
+	"OPCODE_NOP",
+	"OPCODE_SBC_XL_DIR",
+	"OPCODE_SBC_XL_SPREL",
+	"OPCODE_SBC_XL_ZREL",
+	"OPCODE_SBC_XL_ZL",
+	"OPCODE_SBC_XL_XH",
+	"OPCODE_SBC_XL_YL",
+	"OPCODE_SBC_XL_YH",
+	"OPCODE_ADD_XL_IMMD",
+	"OPCODE_ADD_XL_DIR",
+	"OPCODE_ADD_XL_SPREL",
+	"OPCODE_ADD_XL_ZREL",
+	"OPCODE_ADD_XL_ZL",
+	"OPCODE_ADD_XL_XH",
+	"OPCODE_ADD_XL_YL",
+	"OPCODE_ADD_XL_YH",
+	"OPCODE_ADC_XL_IMMD",
+	"OPCODE_ADC_XL_DIR",
+	"OPCODE_ADC_XL_SPREL",
+	"OPCODE_ADC_XL_ZREL",
+	"OPCODE_ADC_XL_ZL",
+	"OPCODE_ADC_XL_XH",
+	"OPCODE_ADC_XL_YL",
+	"OPCODE_ADC_XL_YH",
+	"OPCODE_CP_XL_IMMD",
+	"OPCODE_CP_XL_DIR",
+	"OPCODE_CP_XL_SPREL",
+	"OPCODE_CP_XL_ZREL",
+	"OPCODE_CP_XL_ZL",
+	"OPCODE_CP_XL_XH",
+	"OPCODE_CP_XL_YL",
+	"OPCODE_CP_XL_YH",
+	"OPCODE_OR_XL_IMMD",
+	"OPCODE_OR_XL_DIR",
+	"OPCODE_OR_XL_SPREL",
+	"OPCODE_OR_XL_ZREL",
+	"OPCODE_OR_XL_ZL",
+	"OPCODE_OR_XL_XH",
+	"OPCODE_OR_XL_YL",
+	"OPCODE_OR_XL_YH",
+	"OPCODE_AND_XL_IMMD",
+	"OPCODE_AND_XL_DIR",
+	"OPCODE_AND_XL_SPREL",
+	"OPCODE_AND_XL_ZREL",
+	"OPCODE_AND_XL_ZL",
+	"OPCODE_AND_XL_XH",
+	"OPCODE_AND_XL_YL",
+	"OPCODE_AND_XL_YH",
+	"OPCODE_XOR_XL_IMMD",
+	"OPCODE_XOR_XL_DIR",
+	"OPCODE_XOR_XL_SPREL",
+	"OPCODE_XOR_XL_ZREL",
+	"OPCODE_XOR_XL_ZL",
+	"OPCODE_XOR_XL_XH",
+	"OPCODE_XOR_XL_YL",
+	"OPCODE_XOR_XL_YH",
+	"OPCODE_SRL_DIR",
+	"OPCODE_SRL_SPREL",
+	"OPCODE_SRL_XL",
+	"OPCODE_SRL_ZH",
+	"OPCODE_SLL_DIR",
+	"OPCODE_SLL_SPREL",
+	"OPCODE_SLL_XL",
+	"OPCODE_SLL_ZH",
+	"OPCODE_RRC_DIR",
+	"OPCODE_RRC_SPREL",
+	"OPCODE_RRC_XL",
+	"OPCODE_RRC_ZH",
+	"OPCODE_RLC_DIR",
+	"OPCODE_RLC_SPREL",
+	"OPCODE_RLC_XL",
+	"OPCODE_RLC_ZH",
+	"OPCODE_INC_DIR",
+	"OPCODE_INC_SPREL",
+	"OPCODE_INC_XL",
+	"OPCODE_INC_ZH",
+	"OPCODE_DEC_DIR",
+	"OPCODE_DEC_SPREL",
+	"OPCODE_DEC_XL",
+	"OPCODE_DEC_ZH",
+	"OPCODE_CLR_DIR",
+	"OPCODE_CLR_SPREL",
+	"OPCODE_CLR_XL",
+	"OPCODE_CLR_ZH",
+	"OPCODE_TST_DIR",
+	"OPCODE_TST_SPREL",
+	"OPCODE_TST_XL",
+	"OPCODE_TST_ZH",
+	"OPCODE_PUSH_DIR",
+	"OPCODE_PUSH_SPREL",
+	"OPCODE_PUSH_XL",
+	"OPCODE_PUSH_ZH",
+	"OPCODE_JP_IMMD",
+	"OPCODE_JP_Y",
+	"OPCODE_CALL_IMMD",
+	"OPCODE_CALL_Y",
+	"OPCODE_XCHB_XL_MM_0",
+	"OPCODE_XCHB_XL_MM_1",
+	"OPCODE_XCHB_XL_MM_2",
+	"OPCODE_XCHB_XL_MM_3",
+	"OPCODE_XCHB_XL_MM_4",
+	"OPCODE_XCHB_XL_MM_5",
+	"OPCODE_XCHB_XL_MM_6",
+	"OPCODE_XCHB_XL_MM_7",
+	"OPCODE_LDW_Y_SP",
+	"OPCODE_SUBW_Y_DIR",
+	"OPCODE_SUBW_Y_SPREL",
+	"OPCODE_SUBW_Y_X",
+	"OPCODE_LDW_ISPREL_Y",
+	"OPCODE_SBCW_Y_DIR",
+	"OPCODE_SBCW_Y_SPREL",
+	"OPCODE_SBCW_Y_X",
+	"OPCODE_ADDW_Y_IMMD",
+	"OPCODE_ADDW_Y_DIR",
+	"OPCODE_ADDW_Y_SPREL",
+	"OPCODE_ADDW_Y_X",
+	"OPCODE_ADCW_Y_IMMD",
+	"OPCODE_ADCW_Y_DIR",
+	"OPCODE_ADCW_Y_SPREL",
+	"OPCODE_ADCW_Y_X",
+	"OPCODE_LD_XL_IMMD",
+	"OPCODE_LD_XL_DIR",
+	"OPCODE_LD_XL_SPREL",
+	"OPCODE_LD_XL_ZREL",
+	"OPCODE_LD_XL_IY",
+	"OPCODE_LD_XL_YREL",
+	"OPCODE_LD_XL_XH",
+	"OPCODE_LD_XL_YL",
+	"OPCODE_LD_XL_YH",
+	"OPCODE_LD_XL_ZL",
+	"OPCODE_LD_XL_ZH",
+	"OPCODE_LD_DIR_XL",
+	"OPCODE_LD_SPREL_XL",
+	"OPCODE_LD_ZREL_XL",
+	"OPCODE_LD_IY_XL",
+	"OPCODE_LD_YREL_XL",
+	"OPCODE_PUSH_IMMD",
+	"OPCODE_XCH_XL_SPREL",
+	"OPCODE_XCH_XL_IY",
+	"OPCODE_XCH_YL_YH",
+	"OPCODE_ALTACC4",
+	"OPCODE_ROT_XL_IMMD",
+	"OPCODE_SRA_XL",
+	"OPCODE_DAA_XL",
+	"OPCODE_BOOL_XL",
+	"OPCODE_POP_XL",
+	"OPCODE_THRD_XL",
+	"OPCODE_CAX_IY_ZL_XL",
+	"OPCODE_SWAPOP",
+	"OPCODE_ALTACC1",
+	"OPCODE_ALTACC2",
+	"OPCODE_ALTACC3",
+	"OPCODE_CLRW_DIR",
+	"OPCODE_CLRW_SPREL",
+	"OPCODE_CLRW_ZREL",
+	"OPCODE_CLRW_Y",
+	"OPCODE_INCW_DIR",
+	"OPCODE_INCW_SPREL",
+	"OPCODE_INCW_ZREL",
+	"OPCODE_INCW_Y",
+	"OPCODE_ADCW_DIR",
+	"OPCODE_ADCW_SPREL",
+	"OPCODE_ADCW_ZREL",
+	"OPCODE_ADCW_Y",
+	"OPCODE_SBCW_DIR",
+	"OPCODE_SBCW_SPREL",
+	"OPCODE_SBCW_ZREL",
+	"OPCODE_SBCW_Y",
+	"OPCODE_PUSHW_DIR",
+	"OPCODE_PUSHW_SPREL",
+	"OPCODE_PUSHW_ZREL",
+	"OPCODE_PUSHW_Y",
+	"OPCODE_TSTW_DIR",
+	"OPCODE_TSTW_SPREL",
+	"OPCODE_TSTW_ZREL",
+	"OPCODE_TSTW_Y",
+	"OPCODE_MSK_IY_XL_IMMD",
+	"OPCODE_MUL_Y",
+	"OPCODE_RET",
+	"OPCODE_RETI",
+	"OPCODE_MAD_X_DIR_YL",
+	"OPCODE_MAD_X_SPREL_YL",
+	"OPCODE_MAD_X_ZREL_YL",
+	"OPCODE_MAD_X_IZ_YL",
+	"OPCODE_LDW_Y_IMMD",
+	"OPCODE_LDW_Y_DIR",
+	"OPCODE_LDW_Y_SPREL",
+	"OPCODE_LDW_Y_ZREL",
+	"OPCODE_LDW_Y_YREL",
+	"OPCODE_LDW_Y_IY",
+	"OPCODE_LDW_Y_X",
+	"OPCODE_LDW_Y_D",
+	"OPCODE_LDW_DIR_Y",
+	"OPCODE_LDW_SPREL_Y",
+	"OPCODE_LDW_ZREL_Y",
+	"OPCODE_LDW_X_Y",
+	"OPCODE_LDW_Z_Y",
+	"OPCODE_LDW_IY_X",
+	"OPCODE_LDW_YREL_X",
+	"OPCODE_LDWI_IZ_IY",
+	"OPCODE_JR_D",
+	"OPCODE_DNJNZ_YH_D",
+	"OPCODE_JRZ_D",
+	"OPCODE_JRNZ_D",
+	"OPCODE_JRC_D",
+	"OPCODE_JRNC_D",
+	"OPCODE_JRN_D",
+	"OPCODE_JRNN_D",
+	"OPCODE_ALTACC5",
+	"OPCODE_JRNO_D",
+	"OPCODE_JRSGE_D",
+	"OPCODE_JRSLT_D",
+	"OPCODE_RESERVED_DC",
+	"OPCODE_JRSLE_D",
+	"OPCODE_RESERVED_DE",
+	"OPCODE_JRLE_D",
+	"OPCODE_SRLW_Y",
+	"OPCODE_SLLW_Y",
+	"OPCODE_RRCW_Y",
+	"OPCODE_RLCW_Y",
+	"OPCODE_SRAW_Y",
+	"OPCODE_SLLW_Y_XL",
+	"OPCODE_RRCW_SPREL",
+	"OPCODE_RLCW_SPREL",
+	"OPCODE_PUSHW_IMMD",
+	"OPCODE_POPW_Y",
+	"OPCODE_ADDW_SP_D",
+	"OPCODE_ADDW_Y_D",
+	"OPCODE_XCH_F_SPREL",
+	"OPCODE_LDI_IZ_IY",
+	"OPCODE_SEX_Y_XL",
+	"OPCODE_ZEX_Y_XL",
+	"OPCODE_ORW_Y_IMMD",
+	"OPCODE_ORW_Y_DIR",
+	"OPCODE_ORW_Y_SPREL",
+	"OPCODE_ORW_Y_X",
+	"OPCODE_XCHW_X_IY",
+	"OPCODE_XCHW_Y_SPREL",
+	"OPCODE_INCNW_Y",
+	"OPCODE_DECW_SPREL",
+	"OPCODE_CPW_Y_IMMD",
+	"OPCODE_CAXW_IY_Z_X",
+	"OPCODE_NEGW_Y",
+	"OPCODE_BOOLW_Y",
+	"OPCODE_XORW_Y_IMMD",
+	"OPCODE_XORW_Y_DIR",
+	"OPCODE_XORW_Y_SPREL",
+	"OPCODE_XORW_Y_X",
+	};
+
+
+inline static void init_table_order(uint8_t *table)
+{
+	for(unsigned int i = 0; i < NUM_OPCODES; i++)
+		table[i] = i;
+}
+
+inline static void init_table_random(uint8_t *table)
+{
+	bool assigned_opcodes[NUM_OPCODES] = {};
+	getrandom (table, NUM_OPCODES, 0);
+	table[0] = 0;
+	assigned_opcodes[0] = true;
+	for(unsigned int i = 1; i < NUM_OPCODES; i++)
+	{
+		while(assigned_opcodes[table[i]])
+			table[i]++;
+		assigned_opcodes[table[i]] = true;
+	}
+}
+
+// Modify opcode table by swapping columns or rows in a random table layout.
+inline static void modify_table_random(uint8_t *table)
+{
+	unsigned int rowlength, columnheight;
+	bool swap_rows;
+	rowlength = 0;
+	getrandom (&rowlength, 1, 0);
+	swap_rows = rowlength & 0x10;
+	rowlength %= 9;
+	rowlength = 1 << rowlength;
+	columnheight = NUM_OPCODES / rowlength;
+
+	uint8_t select[2];
+	getrandom (&select, 2, 0);
+	if(!swap_rows) // Swap columns
+	{
+		select[0] %= rowlength;
+		select[1] %= rowlength;
+		printf("%u x %u table, swapping columns %u, %u\n", (unsigned)rowlength, (unsigned)columnheight, (unsigned)select[0], (unsigned)select[1]);
+		for(unsigned int i = 0; i < columnheight; i++)
+		{
+			// trap stays at 0.
+			if(rowlength * i + select[0] == 0 || rowlength * i + select[1] == 0)
+				continue;
+
+			uint8_t tmp = table[rowlength * i + select[0]];
+			table[rowlength * i + select[0]] = table[rowlength * i + select[1]];
+			table[rowlength * i + select[1]] = tmp;
+		}
+	}
+	else // Swap rows
+	{
+		select[0] %= columnheight;
+		select[1] %= columnheight;
+		printf("%u x %u table, swapping rows %u, %u\n", (unsigned)rowlength, (unsigned)columnheight, (unsigned)select[0], (unsigned)select[1]);
+		for(unsigned int i = 0; i < rowlength; i++)
+		{
+			// trap stays at 0.
+			if(rowlength * select[0] + i == 0 || rowlength * select[1] + i == 0)
+				continue;
+
+			uint8_t tmp = table[rowlength * select[0] + i];
+			table[rowlength * select[0] + i] = table[rowlength * select[1] + i];
+			table[rowlength * select[1] + i] = tmp;
+		}
+	}
+}
+
+static void print_table(FILE *f, const uint8_t * table)
+{
+	fprintf(f, "typedef enum logic [7:0] {\n");
+	for(unsigned int i = 0; i < NUM_OPCODES; i++)
+		fprintf(f, "\t%s = 8'h%02x%s\n", opcodenames[table[i]], i, i != 255 ? "," : "");
+	fprintf(f, "} opcode_t;\n");
+}
+
+void help(FILE *f)
+{
+	fprintf(f, "opcodemaptool <command> [n]\n");
+	fprintf(f, "<command>:   create, startrandom <n>, walkrandom <n>, showbest <n>\n");
+	fprintf(f, "create:      init data structures, insert default opcode map\n");
+	fprintf(f, "startrandom: add n random opcode maps\n");
+	fprintf(f, "walkrandom:  add n random opcode maps that are similar to good existin gones\n");
+	fprintf(f, "showbest:    add show best n opcode maps\n");
+}
+
+std::map <std::vector<uint8_t>, unsigned long int> opcodemapstable;
+
+void read_opcodemapstable(void)
+{
+	std::ifstream ifs("opcodemapstable");
+	boost::archive::text_iarchive ia(ifs);
+	ia >> opcodemapstable;
+}
+
+void write_opcodemapstable(void)
+{
+	std::ofstream ofs("opcodemapstable");
+	boost::archive::text_oarchive oa(ofs);
+	oa << opcodemapstable;
+}
+
+void add_opcodemap(const uint8_t *table)
+{
+	opcodemapstable.insert(std::pair <std::vector<uint8_t>, unsigned long int>(std::vector(table, table + NUM_OPCODES), opcodemapstable.size()));
+}
+
+void create_opcodemapsfiles(void)
+{
+	std::map<std::vector<uint8_t>, unsigned long int>::iterator it;
+	for (it = opcodemapstable.begin(); it != opcodemapstable.end(); it++)
+	{
+		std::ostringstream name;
+		name << "opcodemaps/";
+		name << it->second;
+		if (!std::filesystem::exists(name.str().c_str()))
+		{
+			mkdir (name.str().c_str(), 0750);
+			name << "/opcodemap.v";
+			FILE *file = fopen(name.str().c_str(), "wx");
+			print_table(file, it->first.data());
+		}
+	}
+}
+
+std::multimap<unsigned int, std::vector<uint8_t>> sizes;
+
+void get_sizes(void)
+{
+	sizes.clear();
+	std::map<std::vector<uint8_t>, unsigned long int>::iterator it;
+	for (it = opcodemapstable.begin(); it != opcodemapstable.end(); it++)
+	{
+		std::ostringstream name;
+		name << "opcodemaps/";
+		name << it->second;
+		name << "/icesynth2.size";
+		std::ifstream sizefile(name.str().c_str());
+		unsigned int size;
+		sizefile >> size;
+		if(sizefile)
+			sizes.insert(std::pair<unsigned int, std::vector<uint8_t>>(size, it->first));
+		else
+			std::cout << "No size available for " << "opcodemaps/" << it->second << "\n";
+	}
+}
+
+void show_bestsizes(int n)
+{
+	std::multimap<unsigned int, std::vector<uint8_t>>::iterator it;
+	for(it = sizes.begin(); n && it != sizes.end(); it++, n--)
+	{
+		std::cout << "size " << it->first << "\n";
+	}
+}
+
+int main(int argc, char **argv)
+{
+	uint8_t table[NUM_OPCODES];
+	int n = 1;
+
+	if (argc == 3)
+	{
+		long l = strtol(argv[2], 0, 0);
+		if(l < 0 || l == LONG_MAX || l > INT_MAX)
+		{
+			help(stderr);
+			return(-1);
+		}
+		n = l;
+	}
+
+	if (argc == 2 && !strcmp(argv[1], "create"))
+	{
+		if (mkdir ("opcodemaps", 0750))
+		{
+			fprintf (stderr, "Failed to create opcodemaps directory\n");
+			return (-1);
+		}
+		init_table_order(table);
+		add_opcodemap(table);
+		write_opcodemapstable();
+		create_opcodemapsfiles();
+	}
+	else if (argc == 3 && !strcmp(argv[1], "startrandom"))
+	{
+		read_opcodemapstable();
+		for(int i = 0; i < n; i++)
+		{
+			init_table_random(table);
+			add_opcodemap(table);
+		}
+		write_opcodemapstable();
+		create_opcodemapsfiles();
+	}
+	else if (argc == 3 && !strcmp(argv[1], "walkrandom"))
+	{
+		read_opcodemapstable();
+		get_sizes();
+		if (!sizes.size())
+		{
+			std::cout << "Could not read any sizes.\n";
+			return(0);
+		}
+		std::random_device rd;
+		std::geometric_distribution<unsigned long int> dist(1.0 / sizes.size());
+		for(int i = 0; i < n; i++)
+		{
+			unsigned long int r = dist(rd);
+			if (r >= sizes.size())
+				r = 0;
+			std::multimap<unsigned int, std::vector<uint8_t>>::iterator it = sizes.begin();
+			while(r--)
+				it++;
+			memcpy(table, it->second.data(), NUM_OPCODES);
+			unsigned long int d = dist(rd) + 1;
+			if (d > 4)
+				d = 1;
+			while(d--)
+				modify_table_random(table);
+			add_opcodemap(table);
+		}
+		write_opcodemapstable();
+		create_opcodemapsfiles();
+	}
+	else if (argc == 3 && !strcmp(argv[1], "showbest"))
+	{
+		read_opcodemapstable();
+		get_sizes();
+		show_bestsizes(n);
+	}
+	else
+	{
+		help(stderr);
+		return(-1);
+	}
+
+}
+
