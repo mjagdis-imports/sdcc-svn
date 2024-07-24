14d856000a7df2bf07943a005b18ed7b798ac9ef
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2024-07-24 08:01:13 +0000

    Add license headers, adjust distribution parameter for randomized opcodemap testing.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8@14931 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- f8/opcodemaptest/opcodemaptool.cc
+++ f8/opcodemaptest/opcodemaptool.cc
@@ -1,3 +1,23 @@
+/*-------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+   In other words, you are welcome to use, share and improve this program.
+   You are forbidden to forbid anyone else to use, share and improve
+   what you give them.   Help stamp out software-hoarding!
+-------------------------------------------------------------------------*/
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
@@ -493,7 +513,7 @@ int main(int argc, char **argv)
 			return(0);
 		}
 		std::random_device rd;
-		std::geometric_distribution<unsigned long int> dist(1.0 / sizes.size());
+		std::geometric_distribution<unsigned long int> dist(1.0 / 10.0);
 		for(int i = 0; i < n; i++)
 		{
 			unsigned long int r = dist(rd);
