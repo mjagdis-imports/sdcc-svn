916e740417b4ebaf30bc6323955cf6be9c21a7be
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2025-01-06 17:02:47 +0000

    Support UPduino 2 board.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8@15211 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- f8/hardware/boards/boards.mk
+++ f8/hardware/boards/boards.mk
@@ -12,6 +12,9 @@ boards/gatematea1evb_1_nosv.v: boards/gatematea1evb_1.v $(SYSTEM_1_SOURCES)
 boards/icebreaker_2_nosv.v: boards/icebreaker_2.v $(SYSTEM_2_SOURCES)
 	$(SV2V) --exclude=always --exclude=logic $< > $@
 
+boards/upduino2_2_nosv.v: boards/upduino2_2.v $(SYSTEM_2_SOURCES)
+	$(SV2V) --exclude=always --exclude=logic $< > $@
+
 boards/gatematea1evb_2_nosv.v: boards/gatematea1evb_2.v $(SYSTEM_2_SOURCES)
 	$(SV2V) --exclude=always --exclude=logic $< > $@
 
@@ -23,11 +26,14 @@ boards/tangnano9k_2_nosv.v: boards/tangnano9k_2.v $(SYSTEM_2_SOURCES)
 %/icebreaker_1_synth.json: boards/icebreaker_1_nosv.v %/test.vmem
 	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/icebreaker_1_nosv.v; synth_ice40 -abc2 -top icebreaker -json icebreaker_1_synth.json"
 
-%/gatematea1evb_1_synth.v: boards/gatematea1evb_1_nosv.v %/test.vmem
-	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/gatematea1evb_1_nosv.v; synth_gatemate -nomx8 -top gatematea1evb; write_verilog gatematea1evb_1_synth.v"
-
 %/icebreaker_2_synth.json: boards/icebreaker_2_nosv.v %/test.even.vmem %/test.odd.vmem
 	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/icebreaker_2_nosv.v; synth_ice40 -abc2 -top icebreaker -json icebreaker_2_synth.json"
+
+%/upduino2_2_synth.json: boards/upduino2_2_nosv.v %/test.even.vmem %/test.odd.vmem
+	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/upduino2_2_nosv.v; synth_ice40 -abc2 -top icebreaker -json upduino2_2_synth.json"
+
+%/gatematea1evb_1_synth.v: boards/gatematea1evb_1_nosv.v %/test.vmem
+	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/gatematea1evb_1_nosv.v; synth_gatemate -nomx8 -top gatematea1evb; write_verilog gatematea1evb_1_synth.v"
 
 %/gatematea1evb_2_synth.v: boards/gatematea1evb_2_nosv.v %/test.even.vmem %/test.odd.vmem
 	cd $*; $(YOSYS) -p "read_verilog -sv ../../boards/gatematea1evb_2_nosv.v; synth_gatemate -nomx8 -top gatematea1evb; write_verilog gatematea1evb_2_synth.v"
@@ -38,14 +44,20 @@ boards/tangnano9k_2_nosv.v: boards/tangnano9k_2.v $(SYSTEM_2_SOURCES)
 %/icebreaker_1_synth.asc: %/icebreaker_1_synth.json boards/icebreaker.pcf
 	nextpnr-ice40 --up5k --json $*/icebreaker_1_synth.json --pcf boards/icebreaker.pcf --freq 2 --asc $@
 
-%/gatematea1evb_1_synth_00.cfg.bit: %/gatematea1evb_1_synth.v boards/gatematea1evb.ccf
-	cd $*; $(P_R) -cCP -i gatematea1evb_1_synth.v -o gatematea1evb_1_synth -ccf ../../boards/gatematea1evb.ccf
-
 %/icebreaker_2_synth.asc: %/icebreaker_2_synth.json boards/icebreaker.pcf
 	nextpnr-ice40 --up5k --json $*/icebreaker_2_synth.json --pcf boards/icebreaker.pcf --freq 2 --asc $@
 
+%/upduino2_2_synth.asc: %/upduino2_2_synth.json boards/upduino2.pcf
+	nextpnr-ice40 --up5k --json $*/upduino2_2_synth.json --pcf boards/upduino2.pcf --freq 2 --asc $@
+
+%/gatematea1evb_1_synth_00.cfg.bit: %/gatematea1evb_1_synth.v boards/gatematea1evb.ccf
+	cd $*; $(P_R) -cCP -i gatematea1evb_1_synth.v -o gatematea1evb_1_synth -ccf ../../boards/gatematea1evb.ccf
+
 %/gatematea1evb_2_synth_00.cfg.bit: %/gatematea1evb_2_synth.v boards/gatematea1evb.ccf
 	cd $*; $(P_R) -cCP -i gatematea1evb_2_synth.v -o gatematea1evb_2_synth -ccf ../../boards/gatematea1evb.ccf
+
+%/tangnano9k_2.json: %/tangnano9k_2_synth.json boards/tangnano9k.cst
+	$(NEXTPNR_HIMBÄCHEL) --json $< --write $@ --device GW1NR-LV9QN88PC6/I5 --vopt family=GW1N-9C --vopt cst=boards/tangnano9k.cst
 
 %/icebreaker_1_synth.bin: %/icebreaker_1_synth.asc
 	icepack $< $@
@@ -53,8 +65,8 @@ boards/tangnano9k_2_nosv.v: boards/tangnano9k_2.v $(SYSTEM_2_SOURCES)
 %/icebreaker_2_synth.bin: %/icebreaker_2_synth.asc
 	icepack $< $@
 
-%/tangnano9k_2.json: %/tangnano9k_2_synth.json boards/tangnano9k.cst
-	$(NEXTPNR_HIMBÄCHEL) --json $< --write $@ --device GW1NR-LV9QN88PC6/I5 --vopt family=GW1N-9C --vopt cst=boards/tangnano9k.cst
+%/upduino2_2_synth.bin: %/upduino2_2_synth.asc
+	icepack $< $@
 
 %/tangnano9k_2.fs: %/tangnano9k_2.json
 	gowin_pack -d GW1N-9C -o $@ $^
---
+++ f8/hardware/boards/upduino2_2.v
@@ -0,0 +1,34 @@
+`begin_keywords "1800-2009"
+
+`include "../clkdiv.v"
+`include "../system_2.v"
+
+// Test module for use on iCEBreaker FPGA board.
+
+// Default: 9 KB ROM, 6 KB RAM, 2 MHz system clock.
+module icebreaker #(parameter ROMSIZE = 9216, RAMSIZE = 6144, CLKDIV = 6) (
+	inout tri LED_RED_N, inout tri LED_GRN_N, inout tri LED_BLU_N);
+	wire [7:0] gpio0pins, gpio1pins, gpio2pins;
+	wire clk;
+	wire trap;
+	wire power_on_reset;
+
+	SB_HFOSC #(.CLKHF_DIV("0b10")) inthosc(.CLKHFPU(1'b1),.CLKHFEN(1'b1),.CLKHF(CLK)); // Internal oscillator configured for 12 MHz.
+ 
+	// Reset button
+	assign power_on_reset = 0;
+
+	clkdiv #(.CLKDIV(CLKDIV)) clkdiv(.*);
+	system #(.ROMSIZE(ROMSIZE), .RAMSIZE(RAMSIZE)) system(.*);
+
+	// RGB LED
+	assign LED_RED_N = gpio0pins[0];
+	assign gpio0pins[0] = LED_RED_N;
+	assign LED_GRN_N = gpio0pins[1];
+	assign gpio0pins[1] = LED_GRN_N;
+	assign LED_BLU_N = gpio0pins[2];
+	assign gpio0pins[2] = LED_BLU_N;
+endmodule
+
+`end_keywords
+
