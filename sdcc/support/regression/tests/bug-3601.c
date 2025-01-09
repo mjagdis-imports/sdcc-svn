478c8a17f59667445bfa3cc81f1de05f1bdcc73a
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2025-01-09 16:27:08 +0000

    Disable new test on pdk14, due to lack of target memory.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/next@15214 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/support/regression/tests/bug-3601.c
+++ sdcc/support/regression/tests/bug-3601.c
@@ -5,6 +5,7 @@
 
 #include <testfwk.h>
 
+#if defined(__SDCC_pdk13) || defined(__SDCC_pdk14) // Lack of memory.
 unsigned long long f0(unsigned long long i)
 {
 	return (i / ((1ull << 16) | (1ull << 48)));
@@ -14,10 +15,13 @@ long long f1(long long i)
 {
 	return (i / -0x080000000ll);
 }
+#endif
 
 void testBug(void)
 {
+#if defined(__SDCC_pdk13) || defined(__SDCC_pdk14) // Lack of memory.
 	ASSERT (f0((1ull << 16) | (1ull << 48)) == 1);
 	ASSERT (f1(0x080000000ll) == -1);
+#endif
 }
 
