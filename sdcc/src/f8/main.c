d20396c6140eaba7369ae34fc4c3d395c309f412
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2025-08-06 15:56:14 +0000

    Improve f8/f8l startup code. Announce work-in-progress f8l support. Announce work-in-progress Rabbit 4000, 5000, 6000 support.
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/branches/f8l@15551 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/f8/main.c
+++ sdcc/src/f8/main.c
@@ -198,7 +198,7 @@ f8_setDefaultOptions (void)
   options.data_loc = 0x2800; /* Assume a default of 6 KB of RAM for now. */
   options.code_loc = 0x4000;
 
-  options.stack_loc = -1; /* Do not set the stack pointer in software- just use the device-specific reset value. */
+  options.stack_loc = 0x3fff;
 
   options.out_fmt = 'i';        /* Default output format is ihx */
 }
@@ -227,53 +227,80 @@ f8_genExtraArea (FILE *of, bool hasMain)
 static void
 f8_genInitStartup (FILE *of)
 {
-  fprintf (of, "\tldw\ty, #0x%04x ; set stack pointer one above stack\n", options.stack_loc >= 0 ? (options.stack_loc + 1) : 0x4000);
+  fprintf (of, "\tldw\ty, #0x%04x ; set stack pointer one above stack\n", options.stack_loc + 1);
   fprintf (of, "\tldw\tsp, y\n");
 
-  /* Call external startup code */
+  // Call external startup code
   fprintf (of, "\tcall\t#___sdcc_external_startup\n");
 
-  /* If external startup returned non-zero, skip init */
+  // If external startup returned non-zero, skip init
   fprintf (of, "\ttst\txl\n");
   fprintf (of, "\tjrz\t#__sdcc_init_data\n");
   fprintf (of, "\tjp\t#__sdcc_program_startup\n");
 
-  /* Init static & global variables */
+  // Init static & global variables
   fprintf (of, "__sdcc_init_data:\n");
   
-  /* Zeroing memory (required by standard for static & global variables) */
+  // Zeroing memory (required by standard for static & global variables)
   fprintf (of, "\tldw\tz, #l_DATA\n");
-  fprintf (of, "\tjrz\t#00002$\n");
-  fprintf (of, "\tclr\txl\n");
-  fprintf (of, "00001$:\n");
-  fprintf (of, "\tld (s_DATA - 1, z), xl\n");
-  if (TARGET_IS_F8L)
+  if (optimize.codeSpeed)
     {
-      fprintf (of, "\tadd zl, #-1\n");
-      fprintf (of, "\tadc zh, #-1\n");
-      fprintf (of, "\ttstw z\n");
+      fprintf (of, "\tld\txl, zl\n");
+      fprintf (of, "\tbool\txl\n");
+      fprintf (of, "\tjrz\t#00001$\n");
+      fprintf (of, "\tclr\txl\n");
+      fprintf (of, "\tld (s_DATA - 1, z), xl\n");
+      fprintf (of, "\taddw z, #-1\n");
+      fprintf (of, "00001$:\n");
+      fprintf (of, "\ttstw\tz\n");
+      fprintf (of, "\tjrz\t#00003$\n");
+      fprintf (of, "00002$:\n");
+      fprintf (of, "\tclrw (s_DATA - 2, z)\n");
+      fprintf (of, "\taddw z, #-2\n");
+      fprintf (of, "\tjrnz\t#00002$\n");
+      fprintf (of, "00003$:\n");
     }
   else
-    fprintf (of, "\taddw z, #-1\n");
-  fprintf (of, "\tjrnz\t#00001$\n");
-  fprintf (of, "00002$:\n");
+    {
+      fprintf (of, "\tjrz\t#00002$\n");
+      fprintf (of, "\tclr\txl\n");
+      fprintf (of, "00001$:\n");
+      fprintf (of, "\tld (s_DATA - 1, z), xl\n");
+      fprintf (of, "\taddw z, #-1\n");
+      fprintf (of, "\tjrnz\t#00001$\n");
+      fprintf (of, "00002$:\n");
+    }
 
-  /* Copy l_INITIALIZER bytes from s_INITIALIZER to s_INITIALIZED */
+  // Copy l_INITIALIZER bytes from s_INITIALIZER to s_INITIALIZED
   fprintf (of, "\tldw\tz, #l_INITIALIZER\n");
-  fprintf (of, "\tjrz\t#00004$\n");
-  fprintf (of, "00003$:\n");
-  fprintf (of, "\tld\txl, (s_INITIALIZER - 1, z)\n");
-  fprintf (of, "\tld\t(s_INITIALIZED - 1, z), xl\n");
-  if (TARGET_IS_F8L)
+  if (optimize.codeSpeed)
     {
-      fprintf (of, "\tadd zl, #-1\n");
-      fprintf (of, "\tadc zh, #-1\n");
-      fprintf (of, "\ttstw z\n");
+      fprintf (of, "\tld\txl, zl\n");
+      fprintf (of, "\tbool\txl\n");
+      fprintf (of, "\tjrz\t#00004$\n");
+      fprintf (of, "\tld\txl, (s_INITIALIZER - 1, z)\n");
+      fprintf (of, "\tld\t(s_INITIALIZED - 1, z), xl\n");
+      fprintf (of, "\taddw z, #-1\n");
+      fprintf (of, "00004$:\n");
+      fprintf (of, "\ttstw\tz\n");
+      fprintf (of, "\tjrz\t#00006$\n");
+      fprintf (of, "00005$:\n");
+      fprintf (of, "\tldw\ty, (s_INITIALIZER - 2, z)\n");
+      fprintf (of, "\tldw\t(s_INITIALIZED - 2, z), y\n");
+      fprintf (of, "\taddw z, #-2\n");
+      fprintf (of, "\tjrnz\t#00005$\n");
+      fprintf (of, "00006$:\n");
     }
   else
-    fprintf (of, "\taddw z, #-1\n");
-  fprintf (of, "\tjrnz\t#00003$\n");
-  fprintf (of, "00004$:\n");
+    {
+      fprintf (of, "\tjrz\t#00005$\n");
+      fprintf (of, "00004$:\n");
+      fprintf (of, "\tld\txl, (s_INITIALIZER - 1, z)\n");
+      fprintf (of, "\tld\t(s_INITIALIZED - 1, z), xl\n");
+      fprintf (of, "\taddw z, #-1\n");
+      fprintf (of, "\tjrnz\t#00004$\n");
+      fprintf (of, "00005$:\n");
+    }
 }
 
 int
