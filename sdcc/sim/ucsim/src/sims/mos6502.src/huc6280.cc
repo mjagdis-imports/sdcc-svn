eb36ec1ca331c7fb5356ec8727b39c8cb8d54bb9
drdani <drdani@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2026-09-08 11:27:48 +0000

    [ucsim-mos6280] Fix init of MPR0-MPR6
    git-svn-id: https://svn.code.sf.net/p/sdcc/code/trunk@16853 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/sim/ucsim/src/sims/mos6502.src/huc6280.cc
+++ sdcc/sim/ucsim/src/sims/mos6502.src/huc6280.cc
@@ -51,7 +51,7 @@ cl_huc6280::reset(void)
 {
   mpras->write(7, 0);
   //emulate 64k system:
-  for (int i=0; i<7; i++) mpras->write(i,7-i);
+  //for (int i=0; i<7; i++) mpras->write(i,7-i);
   cl_mos65c02s::reset();
 }
 
@@ -84,6 +84,9 @@ cl_huc6280::init(void)
   mk_mvar(mpras, 5, "MPR5", "Mapping Register 5");
   mk_mvar(mpras, 6, "MPR6", "Mapping Register 6");
   mk_mvar(mpras, 7, "MPR7", "Mapping Register 7");
+  // power-on values for MAP registers
+  for (i=0;i<7;i++)
+    mpras->write(i, 0xff);
   return 0;
 }
 
